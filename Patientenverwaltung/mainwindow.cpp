ad235a9712e5b9ee853a55ad559482e3c05e8d7e
Jasmin <jsoelling@rheinahrcampus.de>
2021-10-17 22:17:47 +0200

    Bilder in Ressource-File

--- Patientenverwaltung/mainwindow.cpp
+++ Patientenverwaltung/mainwindow.cpp
@@ -18,32 +18,29 @@ MainWindow::MainWindow(QWidget *parent)
 
     //Icons
 
-    QIcon hinzufuegen;
     //https://www.clipartmax.com/download/m2i8m2Z5K9Z5N4d3_plus-icons-no-attribution-add-icon-green/
-    hinzufuegen.addFile("/home/dbuser/Dokumente/Patientenverwaltung/Patientenverwaltung/clipart1719902.png");
-    QIcon loeschen;
+    QIcon* hinzufuegen = new QIcon(":/Bilder/clipart1719902.png");
     //https://www.clipartmax.com/download/m2i8b1N4m2N4m2b1_trash-can-clip-art/
-    loeschen.addFile("/home/dbuser/Dokumente/Patientenverwaltung/Patientenverwaltung/clipart1976610.png");
-    QIcon aendern;
+    QIcon* loeschen = new QIcon(":/Bilder/clipart1976610.png");
     //https://www.clipartmax.com/download/m2i8i8G6i8G6K9i8_pencil-and-paper-clipart-school-pencil-clip-art/
-    aendern.addFile("/home/dbuser/Dokumente/Patientenverwaltung/Patientenverwaltung/clipart209647.png");
-    QIcon suche;
+    QIcon* aendern = new QIcon(":/Bilder/clipart209647.png");
     //https://www.clipartmax.com/download/m2K9A0b1m2d3G6K9_clipart-magnifying-glass-black-clipart/
-    suche.addFile("/home/dbuser/Dokumente/Patientenverwaltung/Patientenverwaltung/clipart28741.png");
-    QIcon export_icon;
+    QIcon* suche = new QIcon(":/Bilder/clipart28741.png");
     //https://www.clipartmax.com/download/m2i8H7m2G6d3A0N4_email-free-clip-art-envelope-clipart-transparent-background/
-    export_icon.addFile("/home/dbuser/Dokumente/Patientenverwaltung/Patientenverwaltung/clipart611030.png");
+    QIcon* export_icon = new QIcon(":/Bilder/clipart611030.png");
+
     QSize iconSize;
     iconSize.setWidth(60);
     iconSize.setHeight(60);
-    ui->hinzufuegen_button->setIcon(hinzufuegen);
-    ui->loeschen_button->setIcon(loeschen);
+
+    ui->hinzufuegen_button->setIcon(*hinzufuegen);
+    ui->loeschen_button->setIcon(*loeschen);
     ui->loeschen_button->setIconSize(iconSize);
-    ui->aendern_button->setIcon(aendern);
+    ui->aendern_button->setIcon(*aendern);
     ui->aendern_button->setIconSize(iconSize);
-    ui->suchen_button->setIcon(suche);
+    ui->suchen_button->setIcon(*suche);
     ui->suchen_button->setIconSize(iconSize);
-    ui->export_button->setIcon(export_icon);
+    ui->export_button->setIcon(*export_icon);
     ui->export_button->setIconSize(iconSize);
 
 
